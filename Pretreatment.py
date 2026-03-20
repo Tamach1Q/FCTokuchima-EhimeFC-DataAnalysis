0ee040ffd3bab248f44fd73de90ac6ec8fd53369
Tamach1Q <akihiro77akihiro@gmail.com>
2026-03-20 18:32:41 +0900

    new

--- Pretreatment.py
+++ Pretreatment.py
@@ -1,2 +1,3 @@
 import gdown
 folder_url = 'https://drive.google.com/drive/folders/15hhq0OywBt77uy9iydunzXsnyWMxZPhe?usp=drive_link'
+gdown.download_folder(folder_url, output='ehime_fc_data', quiet=False, use_cookies=False)
